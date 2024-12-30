583057d417484abce262c5276c33d289c9dd3e95
Serif-C <92169062+Serif-C@users.noreply.github.com>
2024-12-29 20:45:53 -0500

    Started working on Player Sanity
    wrong branch lol

---
+++ Mannequin Horror/Assets/Scripts/Player/Sanity Manager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanityManager : MonoBehaviour
+{
+    /* 
+     * Sanity:
+     * - Numerical value hidden from player
+     * - Responsible for event frequency (Obvious and Hidden Clues)
+     * - Gradually decreases with Time
+     * - Decreases more rapidly when:
+     *      * player is in complete darkness
+     *      * under the effect of events (i.e., when eye sight is covered by a mannequin's hands)
+     */
+
+    //[Header("References")]
+
+    [Header("Sanity Status")]
+    [SerializeField] private float maxSanityValue = 100f;
+    [SerializeField] private float currentSanityValue = 0f;
+    [SerializeField] private float sanityDecreaseRate = 0.05f;
+    [SerializeField] private bool isInDarkness = false;
+    [SerializeField] private bool isInfluenced = false;
+
+    private void Start()
+    {
+        currentSanityValue = maxSanityValue;
+    }
+
+    private void Update()
+    {
+        // Decrease sanity at an even interval
+        currentSanityValue -= sanityDecreaseRate * Time.deltaTime;
+    }
+
+    private bool IsInDarkness()
+    {
+        // Probably use Candles/Flashlight to determine this
+        return false;
+    }
+
+    private bool InInfluenced()
+    {
+        return false;
+    }
+}
